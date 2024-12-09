c76da849f60a3cb33e0222a7f21ba20774125a84
tuananh.204 <anhthu09112k5@gmail.com>
2024-12-10 00:06:31 +0700

    change icon

--- src/components/layout/client/header-client/index.jsx
+++ src/components/layout/client/header-client/index.jsx
@@ -588,10 +588,21 @@ const HeaderClient = () => {
                                     href="thong-tin-tai-khoan"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="user"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-user me-2 text-body align-bottom"
+                                    >
+                                      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                                      <circle cx={12} cy={7} r={4} />
+                                    </svg>
                                     <span>Profile</span>
                                   </a>
                                 </li>
@@ -602,10 +613,28 @@ const HeaderClient = () => {
                                     href="#!"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="lock"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-lock me-2 text-body align-bottom"
+                                    >
+                                      <rect
+                                        x={3}
+                                        y={11}
+                                        width={18}
+                                        height={11}
+                                        rx={2}
+                                        ry={2}
+                                      />
+                                      <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+                                    </svg>
                                     Posts &amp; Activity
                                   </a>
                                 </li>
@@ -615,10 +644,21 @@ const HeaderClient = () => {
                                     href="#!"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="settings"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-settings me-2 text-body align-bottom"
+                                    >
+                                      <circle cx={12} cy={12} r={3} />
+                                      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
+                                    </svg>
                                     Settings &amp; Privacy{" "}
                                   </a>
                                 </li>
@@ -628,10 +668,27 @@ const HeaderClient = () => {
                                     href="#!"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="help-circle"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-help-circle me-2 text-body align-bottom"
+                                    >
+                                      <circle cx={12} cy={12} r={10} />
+                                      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+                                      <line
+                                        x1={12}
+                                        y1={17}
+                                        x2="12.01"
+                                        y2={17}
+                                      />
+                                    </svg>
                                     Help Center
                                   </a>
                                 </li>
@@ -641,10 +698,22 @@ const HeaderClient = () => {
                                     href="#!"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="globe"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-globe me-2 text-body align-bottom"
+                                    >
+                                      <circle cx={12} cy={12} r={10} />
+                                      <line x1={2} y1={12} x2={22} y2={12} />
+                                      <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+                                    </svg>
                                     Language
                                   </a>
                                 </li>
@@ -658,10 +727,23 @@ const HeaderClient = () => {
                                     href="#!"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="user-plus"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="16px"
+                                      height="16px"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      strokeWidth={2}
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      className="feather feather-user-plus me-2 text-body align-bottom"
+                                    >
+                                      <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+                                      <circle cx="8.5" cy={7} r={4} />
+                                      <line x1={20} y1={8} x2={20} y2={14} />
+                                      <line x1={23} y1={11} x2={17} y2={11} />
+                                    </svg>
                                     Add another account
                                   </a>
                                 </li>
@@ -674,9 +756,22 @@ const HeaderClient = () => {
                                   href="#"
                                   onClick={handleLogout}
                                 >
-                                  <span className="me-2" data-feather="log-out">
-                                    {" "}
-                                  </span>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16px"
+                                    height="16px"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="feather feather-log-out me-2"
+                                  >
+                                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                                    <polyline points="16 17 21 12 16 7" />
+                                    <line x1={21} y1={12} x2={9} y2={12} />
+                                  </svg>
                                   Sign out
                                 </a>
                               </div>
@@ -719,10 +814,17 @@ const HeaderClient = () => {
                                     href="dang-ky"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="user"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="24px"
+                                      height="24px"
+                                      viewBox="0 0 24 24"
+                                      fill="currentColor"
+                                      className="bi bi-person-add"
+                                    >
+                                      <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                                      <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                                    </svg>
                                     <span>Đăng ký</span>
                                   </a>
                                 </li>
@@ -733,10 +835,17 @@ const HeaderClient = () => {
                                     href="dang-nhap"
                                   >
                                     {" "}
-                                    <span
-                                      className="me-2 text-body align-bottom"
-                                      data-feather="globe"
-                                    />
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="24px"
+                                      height="24px"
+                                      viewBox="0 0 24 24"
+                                      fill="currentColor"
+                                      className="bi bi-person-add"
+                                    >
+                                      <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                                      <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                                    </svg>
                                     Đăng nhập
                                   </a>
                                 </li>
