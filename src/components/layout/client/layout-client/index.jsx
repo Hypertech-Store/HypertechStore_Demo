9255156396d1733a92a18fb39dac046533b03eab
tuananh.204 <anhthu09112k5@gmail.com>
2025-01-12 21:41:47 +0700

    settime loading admin client

--- src/components/layout/client/layout-client/index.jsx
+++ src/components/layout/client/layout-client/index.jsx
@@ -21,7 +21,7 @@ const LayoutClient = () => {
     // Giả lập việc tải dữ liệu (có thể thay bằng các API request thực tế)
     const timer = setTimeout(() => {
       setIsLoading(false); // Giả lập đã tải dữ liệu xong
-    }, 10000); // Bạn có thể thay đổi thời gian này tùy vào thời gian tải thực tế
+    }, 6000); // Bạn có thể thay đổi thời gian này tùy vào thời gian tải thực tế
 
     return () => clearTimeout(timer); // Dọn dẹp khi component unmount
   }, []);
